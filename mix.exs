0c482b769d45104d2053078db36ee6ab1ae85039
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:04 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -45,16 +45,15 @@ defmodule Membrane.G711.FFmpeg.Mixfile do
       {:membrane_raw_audio_format, "~> 0.12.0"},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
       {:membrane_raw_audio_parser_plugin, "~> 0.4.0", only: :test},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
